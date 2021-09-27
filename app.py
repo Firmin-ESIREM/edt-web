41d4929ae3401df508b57cb8e1eae8929915fded
Firmin Launay <hey@firminlaunay.me>
2021-09-27 21:35:59 +0200

    Changes

--- app.py
+++ app.py
@@ -1,37 +1,46 @@
 from flask import Flask, request, render_template, send_file
-from werkzeug.exceptions import NotFound
+from flask_cors import CORS
+from werkzeug.exceptions import NotFound, BadRequest
 from os.path import isfile
 from bjoern import run
+from requests import get
 
 app = Flask(__name__)
+CORS(app)
 
 
 ELEMENTS = [
     {
         "name": "TP2A",
-        "url": "google.fr"
+        "url": ""
     }
 ]
 
 
 @app.route('/')
 def list_edt():
-    print(request.path)
     return render_template("index.j2", elements=ELEMENTS)
 
 
 @app.route("/edt/")
 def show_edt():
-    return "gégé"
+    group = request.args.get("group")
+    if group == None:
+        raise BadRequest
+    if isfile("ics/" + group + ".ics"):
+        return render_template("edt.j2", group=group)
+    raise NotFound
 
 
 @app.route("/ics/")
 def get_ics():
-    path = "ics/" + request.args.get("file")
+    file = request.args.get("file")
+    if file == None:
+        raise BadRequest
+    path = "ics/" + file + ".ics"
     if isfile(path):
         return send_file(path)
-    else:
-        raise NotFound
+    raise NotFound
 
 
 if __name__ == '__main__':
